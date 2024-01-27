2164d733ce52db1985c1fce69b6afe3c265cb1c2
Ander Goirigolzarri <ander.goiritur@gmail.com>
2024-01-27 16:17:42 +0100

    Update on documentation for Encrypt

--- src/security/Encrypt.java
+++ src/security/Encrypt.java
@@ -1,20 +1,42 @@
 package security;
 
 import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.security.InvalidKeyException;
 import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
 import java.security.PublicKey;
 import java.security.Security;
+import java.security.spec.InvalidKeySpecException;
 import java.security.spec.X509EncodedKeySpec;
 import java.util.Base64;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 /**
+ * The Encrypt class provides a method to encrypt a password using the ECIES
+ * algorithm with a public key. The encrypted password is returned in Base64
+ * encoding format. Bouncy Castle provider is used for cryptographic operations.
  *
  * @author Ander Goirigolzarri Iturburu
  */
 public class Encrypt {
 
+    private static final Logger LOGGER = Logger.getLogger(Encrypt.class.getName());
+
+    /**
+     * Encrypts the given password using the ECIES algorithm with a public key.
+     *
+     * @param password The password to be encrypted.
+     * @return The encrypted password in Base64 encoding format, or null if
+     * encryption fails.
+     */
     public String encrypt(String password) {
         String encryptedPassword = null;
         try {
@@ -23,11 +45,13 @@ public class Encrypt {
 
             byte[] publicKeyBytes;
             try (FileInputStream fis = new FileInputStream(
-                    System.getProperty("user.home") + "\\esportshub\\security\\publicKey.der")) {
+                    Paths.get(System.getProperty("user.home"),
+                            "\\esportshub\\security", "publicKey.der")
+                            .toString()
+            )) {
                 publicKeyBytes = new byte[fis.available()];
                 fis.read(publicKeyBytes);
             }
-
             X509EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(publicKeyBytes);
             KeyFactory keyFactory = KeyFactory.getInstance("EC");
             PublicKey publicKey = keyFactory.generatePublic(publicKeySpec);
@@ -41,8 +65,8 @@ public class Encrypt {
 
             // Codifica los bytes encriptados a Base64
             encryptedPassword = Base64.getEncoder().encodeToString(encryptedBytes);
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (IOException | InvalidKeyException | NoSuchAlgorithmException | InvalidKeySpecException | BadPaddingException | IllegalBlockSizeException | NoSuchPaddingException e) {
+            LOGGER.log(Level.SEVERE, "Error occurred while encrypting password", e);
         }
         return encryptedPassword;
     }
