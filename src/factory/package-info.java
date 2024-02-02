7a93f077e00d82fe77e50c8db1fadce6d6b84f8f
Ander Goirigolzarri <ander.goiritur@gmail.com>
2024-02-02 10:31:52 +0100

    Package info

---
+++ src/factory/package-info.java
@@ -0,0 +1,6 @@
+/*
+ * Provides a factory for creating instances of interfaces related to the application logic.
+ * The classes in this package follow the Factory Method design pattern, allowing the creation
+ * of objects to be delegated to static methods.
+ */
+package factory;
---
+++ src/security/package-info.java
@@ -0,0 +1,5 @@
+/*
+ * This package contains classes related to security operations, including encryption,
+ * public key handling, and hashing.
+ */
+package security;
